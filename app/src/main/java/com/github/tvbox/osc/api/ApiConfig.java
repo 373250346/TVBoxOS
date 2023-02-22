52b8733aae3e033f66a863d1a7d3cb6d90da06ba
373250346 <54652985+373250346@users.noreply.github.com>
2023-02-22 23:24:38 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -127,7 +127,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://freed.yuanhsing.cf/TVBox/meowcf.json");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "http://piaoyuwang.gicp.net:5002/meowcf.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
